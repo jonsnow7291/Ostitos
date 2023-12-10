b0655eeca6daf41b56f54210d73e4efc49d76e81
JDCHAPARRO <juanchaparro729@gmail.com>
2023-12-10 00:01:29 -0500

    Cambios Inicio trabajador

--- Codigos/InicioTrabajador.php
+++ Codigos/InicioTrabajador.php
@@ -3,9 +3,9 @@
 
 <head>
     <?php 
+    session_start();
     include("Conexion.php");
-    $id=$_GET['IdUsu'];
-    $sqlruta="select * from usuario where IdUsu =".$id;
+    $sqlruta="select * from usuario where IdUsu =". $_SESSION['IdUsu'];
     $resultadoUsuario=mysqli_query($conect,$sqlruta);
     ?>
     <meta charset="UTF-8">
@@ -42,6 +42,9 @@
 
                             echo "<a class='nav-link'>Empleado:".$row['NicknameUsu']."!!</a>";
                             }  ?>
+                    </li>
+                    <li class="nav-item">    
+                        <a href="/Codigos/CerrarSesion.php" class="nav-link">CerrarSesion</a>
                     </li>
                     <li class="nav-item">    
                         <a href="/Codigos/CerrarSesion.php" class="nav-link">CerrarSesion</a>
