fc24f7806556125496c2745ff5d5ad349160f516
DaniLamprea <danilampreasabogal2005.05@gmail.com>
2023-12-15 19:08:20 -0500

    Revert "Revert "Mas correcciones""
    This reverts commit a0dba4ab47bd6bcfaad61fd7dc843e62359e2b67.

--- Codigos/css/app.js
+++ Codigos/css/app.js
@@ -6,16 +6,29 @@ let body = document.querySelector('body');
 let total = document.querySelector('.total');
 let quantity = document.querySelector('.quantity');
 
-openShopping.addEventListener('click', ()=>{
+openShopping.addEventListener('click', () => {
     body.classList.add('active');
-})
-closeShopping.addEventListener('click', ()=>{
-    body.classList.remove('active');
-})
+});
 
-let listCards  = [];
-function initApp(){
-    products.forEach((value, key) =>{
+closeShopping.addEventListener('click', () => {
+    body.classList.remove('active');
+});
+
+let listCards = [];
+
+// Fetch products from the server
+async function fetchProducts() {
+    try {
+        const response = await fetch('/api/products');
+        const products = await response.json();
+        initApp(products);
+    } catch (error) {
+        console.error('Error fetching products:', error);
+    }
+}
+
+function initApp(products) {
+    products.forEach((value, key) => {
         let newDiv = document.createElement('div');
         newDiv.classList.add('item');
         newDiv.innerHTML = `
@@ -24,16 +37,11 @@ function initApp(){
             <div class="price">${value.price.toLocaleString()}</div>
             <button onclick="addToCard(${key})">Add To Card</button>`;
         list.appendChild(newDiv);
-    })
+    });
 }
-initApp();
-function addToCard(key){
-    if(listCards[key] == null){
-        listCards[key] = JSON.parse(JSON.stringify(products[key]));
-        listCards[key].quantity = 1;
-    }
-    reloadCard();
-}
+
+fetchProducts();
+
 function reloadCard(){
     listCard.innerHTML = '';
     let count = 0;
